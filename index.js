3872ed7c5f1770bb6c5f434616770248df0be954
Stef van Wijchen <stefvanwijchen@gmail.com>
2021-10-07 14:32:13 +0200

    fIrST CommIt

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("*", (req, res) => {
+  res.send(mockingCase(req.query.text || ""));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+function mockingCase(str) {
+  return Array.from(str)
+    .map((char) =>
+      Math.round(Math.random()) ? char.toLowerCase() : char.toUpperCase()
+    )
+    .join("");
+}
